8b4e68ebd0e244ecf388ac332b2ca2f173108b88
Эмиль Ахияров <emil.ahiarov@gmail.com>
2024-09-22 02:38:05 +0500

    private key id added

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ function NodeGoogleSheets(keyMass, fun) {
 		credentials: {
             type: "service_account",
             project_id: process.env.GOOGLE_PROJECT_ID,
-            private_key_id: process.env.GOOGLE_PROJECT_ID,
+            private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
             private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // заменяем \n на фактический перевод строки
             client_email: process.env.GOOGLE_CLIENT_EMAIL,
             client_id: process.env.GOOGLE_CLIENT_ID,
